9a9c3a62f88118e2993680c8aca2efe8c2690488
Halbach Johannes (IFAG PSS SIS SWT AI) <Johannes.Halbach@infineon.com>
2023-09-04 10:14:47 +0200

    added basic react app, from create-react-pp

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        Welcome!
+      </header>
+      <div className="Background">
+        <p className="App-tile">
+          testtest
+        </p>
+      </div>  
+    </div>
+  );
+}
+
+export default App;
